c4c7ac2cf739e9be229b2bb014ef271c01dfa324
unknown <habtamuanbese@gmail.com>
2024-11-23 21:14:58 -0600

    First  netflix-clone commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from '../../component/Header/Header';
+import Footer from '../../component/Footer/Footer';
+import Banner from '../../component/Banner/Banner';
+import RowList from '../../component/Rows/RowList/RowList';
+const Home = () => {
+  return (
+    <>
+     <Header/>
+     <Banner/>
+     <RowList/>
+     <Footer/>
+    </>
+  );
+};
+
+export default Home;
